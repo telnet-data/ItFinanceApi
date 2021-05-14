f702f85cf033b0a08971763f017d05946a21a791
Simone Not <smn.not@outlook.com>
2021-05-14 15:15:22 +0200

    v0.0.4 fix in default value

--- it_finance_api/models/base.py
+++ it_finance_api/models/base.py
@@ -43,12 +43,12 @@ class AddressData:
         self.cap: str = data.get('cap', '')
         self.city: str = data.get('city', '')
         self.city_code: str = data.get('cityCode', '')
-        self.city_code_istat: str = data.get('cityCodeIstat')
-        self.name: str = data.get('name')
-        self.number: str = data.get('number')
-        self.place_name: str = data.get('placeName')
-        self.province: str = data.get('province')
-        self.town: str = data.get('town')
+        self.city_code_istat: str = data.get('cityCodeIstat', '')
+        self.name: str = data.get('name', '')
+        self.number: str = data.get('number', '')
+        self.place_name: str = data.get('placeName', '')
+        self.province: str = data.get('province', '')
+        self.town: str = data.get('town', '')
 
 
 class KeyValueData:
